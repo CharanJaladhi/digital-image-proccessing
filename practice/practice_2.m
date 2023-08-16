12e6c142d7b1eded598b921ec292ab7d28158057
Charan Jaladhi <137475709+CharanJaladhi@users.noreply.github.com>
2023-08-16 11:13:48 +0530

    Delete practice directory

--- practice/practice_2.m
+++
@@ -1,9 +0,0 @@
-img=imread('C:\Users\vignan\Desktop\dip\1st_exp\bb.jpg');
-subplot(2,2,1);
-imshow(img);
-title('Original Image');
-i=rgb2gray(img);
-c=edge(i,'canny');
-subplot(2,2,2);
-imshow(c);
-title('Edge Detection');
